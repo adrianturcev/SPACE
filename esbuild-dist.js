762781769191d1669e57d72db221be56b67e32e6
Adrian Turcev <adrian.turcev@planviii.com>
2024-08-13 14:28:08 +0200

    [cha] fixed build naming

--- esbuild-dist.js
+++ esbuild-dist.js
@@ -4,7 +4,7 @@ const esbuild = require('esbuild');
     const ctx = await esbuild.context(
         {
             entryPoints: ['main.js'],
-            outfile: 'client.min.js',
+            outfile: 'space.min.js',
             bundle: true,
             // sourcemap: true,
             minify: true,
