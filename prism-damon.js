86b38def44942ac09d2c4fc5d9b729aef555c04b
Adrian Turcev <adrian.turcev@planviii.com>
2024-12-21 02:07:35 +0100

    [cha] syntax highlighing

--- prism-damon.js
+++ prism-damon.js
@@ -1,32 +1,35 @@
 Prism.languages.damon = {
-	'list-hyphen': {
-		pattern: /(^\s*)(?:[*-])/m,
-		lookbehind: true,
-		greedy: true
-	},
-	'string': {
-		pattern: /(^|[^\\])"(?:\\.|[^\\"\r\n])*"(?!\s*:)/,
-		lookbehind: true,
-		greedy: true
-	},
-	'comment': {
-		pattern: /\/\/.*|\/\*[\s\S]*?(?:\*\/|$)\n/,
-		greedy: true
-	},
-	'number': /-?\b\d+(?:\.\d+)?(?:e[+-]?\d+)?\b/i,
-	'operator': /:/,
-	'boolean': /\b(?:false|true)\b/,
-	'null': {
-		pattern: /\bnull\b/,
-		alias: 'keyword'
-	},
-	'map': {
-		pattern: /(\{\})\n/,
-	},
-	'list': {
-		pattern: /(\[\])\n/,
-	},
-	'punctuation': /[\{\}\[\],]/,
+    'list-hyphen': {
+        pattern: /(^\s*)(?:[*-])/m,
+        lookbehind: true,
+        greedy: true
+    },
+    'key': {
+        pattern: /.+(?=(: (true *$|false *$|null *$|\".*\" *$|\\{.*\\} *$|\\[.*] *$|-?(?:0|[1-9]\\d*)(?:(?:\\.\\d+)?(?:[eE][+-]?\\d+)?)? *$)))/,
+    },
+    'string': {
+        pattern: /(^|[^\\])"(?:\\.|[^\\"\r\n])*"(?!\s*:)/,
+        lookbehind: true,
+        greedy: true
+    },
+    'comment': {
+        pattern: /\/\/.*|\/\*[\s\S]*?(?:\*\/|$)\n/,
+        greedy: true
+    },
+    'number': /-?\b\d+(?:\.\d+)?(?:e[+-]?\d+)?\b/i,
+    'operator': /:/,
+    'boolean': /\b(?:false|true)\b/,
+    'null': {
+        pattern: /\bnull\b/,
+        alias: 'keyword'
+    },
+    'map': {
+        pattern: /(\{\})\n/,
+    },
+    'list': {
+        pattern: /(\[\])\n/,
+    },
+    'punctuation': /[\{\}\[\],]/,
 };
 
 Prism.languages.webmanifest = Prism.languages.damon;
